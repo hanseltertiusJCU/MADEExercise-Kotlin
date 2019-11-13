c938a8de7311edaa770844bfd6b71b7b3afe2c70
Hansel Tertius <hansel.tertius@gmail.com>
2019-11-13 16:25:18 +0700

    Implement changing one fragment to another in the same Activity

--- MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/CategoryFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/CategoryFragment.kt
@@ -12,33 +12,27 @@ import androidx.fragment.app.FragmentManager
 /**
  * A simple [Fragment] subclass.
  */
-class HomeFragment : Fragment(), View.OnClickListener {
+class CategoryFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_category, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnCategory : Button = view.findViewById(R.id.btn_category)
-        btnCategory.setOnClickListener(this)
+        val btnDetailCategory : Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
-        if(v.id == R.id.btn_category) {
-            val mCategoryFragment = CategoryFragment()
-            val mFragmentManager = fragmentManager as FragmentManager
+        if(v.id == R.id.btn_detail_category) {
 
-            mFragmentManager
-                .beginTransaction()
-                .replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
-                .addToBackStack(null)
-                .commit()
         }
     }
+
 
 }
